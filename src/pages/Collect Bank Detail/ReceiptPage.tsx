19c63cb095a36e350f6b5e84372a182911286e40
Umerkhan26 <114725165+Umerkhan26@users.noreply.github.com>
2025-11-07 22:38:48 +0500

    updated

--- src/pages/Collect Bank Detail/ReceiptPage.tsx
+++ src/pages/Collect Bank Detail/ReceiptPage.tsx
@@ -41,13 +41,13 @@ const ReceiptPage: React.FC = () => {
     doc.text(`Premium Title: ${bankPremium.title}`, 20, 70);
     doc.text(`Points Required: ${bankPremium.points_required}`, 20, 80);
     doc.text(`Points Used: ${userHistory.points_used}`, 20, 90);
-    doc.text(`Enrolled Users: ${bankPremium.enrolled_users.length}`, 20, 100);
-    doc.text(`Total Redemptions: ${bankPremium.redemptions.length}`, 20, 110);
-    doc.text(`Brand ID: ${bankPremium.brand}`, 20, 120);
+    // doc.text(`Enrolled Users: ${bankPremium.enrolled_users.length}`, 20, 100);
+    // doc.text(`Total Redemptions: ${bankPremium.redemptions.length}`, 20, 110);
+    // doc.text(`Brand ID: ${bankPremium.brand}`, 20, 120);
     doc.text(`User: ${user.username}`, 20, 130);
     doc.text(`User ID: ${user.userId}`, 20, 140);
     doc.text(`User Brand ID: ${user.brandId}`, 20, 150);
-    doc.text(`Remaining Points: ${user.remaining_brand_points}`, 20, 160);
+    doc.text(`Remaining Points: ${user.remaining_total_brand_points}`, 20, 160);
     doc.text(`Transaction Type: ${userHistory.type}`, 20, 170);
     doc.text(`Description: ${userHistory.description}`, 20, 180);
     doc.text(`Date: ${new Date().toLocaleString()}`, 20, 190);
@@ -71,12 +71,12 @@ const ReceiptPage: React.FC = () => {
           <Value>{bankPremium.points_required}</Value>
           <Label>Points Used:</Label>
           <Value>{userHistory.points_used}</Value>
-          <Label>Enrolled Users:</Label>
-          <Value>{bankPremium.enrolled_users.length}</Value>
-          <Label>Total Redemptions:</Label>
-          <Value>{bankPremium.redemptions.length}</Value>
-          <Label>Brand ID:</Label>
-          <Value>{bankPremium.brand}</Value>
+          {/* <Label>Enrolled Users:</Label>
+          <Value>{bankPremium.enrolled_users.length}</Value> */}
+          {/* <Label>Total Redemptions:</Label>
+          <Value>{bankPremium.redemptions.length}</Value> */}
+          {/* <Label>Brand ID:</Label>
+          <Value>{bankPremium.brand}</Value> */}
           <Label>User:</Label>
           <Value>{user.username}</Value>
           <Label>User ID:</Label>
@@ -84,7 +84,7 @@ const ReceiptPage: React.FC = () => {
           <Label>User Brand ID:</Label>
           <Value>{user.brandId}</Value>
           <Label>Remaining Points:</Label>
-          <Value>{user.remaining_brand_points}</Value>
+          <Value>{user.remaining_total_brand_points}</Value>
           <Label>Transaction Type:</Label>
           <Value>{userHistory.type}</Value>
           <Label>Description:</Label>
